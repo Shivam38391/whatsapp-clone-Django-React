eee985e02285700bf425c5b77d8eb2683246d7d7
shivam sharma <shivamsharma38391@gmail.com>
2023-11-05 22:22:57 +0530

    tailwind addes and login form design is improved

--- Chat-app-frontend/frontend/src/App.jsx
+++ Chat-app-frontend/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-// import './App.css'
+import './App.css'
 import Register from './components/Register'
 import Login from './components/Login'
 
--- Chat-app-frontend/frontend/src/components/Login.jsx
+++ Chat-app-frontend/frontend/src/components/Login.jsx
@@ -1,101 +1,164 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
 
-
 function Login() {
+  const [message, setmessage] = useState("");
 
+  const [formData, setformData] = useState({
+    email: "",
+    password: "",
+  });
 
+  const handleFormSubmit = () => {
+    const baseurl = "http://127.0.0.1:8000/";
 
-    const [message, setmessage] = useState("")
+    async function postJSON() {
+      const options = {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
 
-    const [formData, setformData] = useState({
-                                          "email": "",
-                                          "password": "",
-                                        })
-  
+      try {
+        const response = await fetch(`${baseurl}login/`, options);
 
+        const result = await response.json();
 
+        console.log("Success:", result);
+        console.log(result["email"][0]);
 
-
-    const handleFormSubmit = () => {
-
-        const baseurl = "http://127.0.0.1:8000/";
-    
-    
-        async function postJSON() {
-    
-          const options = {
-    
-            method: "POST", // or 'PUT'
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify( formData ),
-          }
-    
-          try {
-            const response = await fetch( `${baseurl}login/` , options);
-        
-            const result = await response.json();
-    
-            console.log("Success:", result);
-            console.log(result["email"][0]);
-    
-            setmessage(result["email"][0])
-    
-          } catch (error) {
-            console.error("Error:", error);
-          }
-        }
-        
-        // const data = { username: "example" };
-        postJSON();
-        
-    
-    
+        setmessage(result["email"][0]);
+      } catch (error) {
+        console.error("Error:", error);
       }
+    }
 
-
-
+    // const data = { username: "example" };
+    postJSON();
+  };
 
   return (
     <>
-      <div>Login page</div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
 
-      <div  className=" container text-center ">
-        <h3 className="info"> {message} </h3>
+          <h3 className="info"> {message} </h3>
+        </div>
 
-        <TextField
-          id="email"
-          label="email"
-          type="email"
-          color="secondary"
-          focused
-          margin="normal"
-          onChange={(e) => setformData({ ...formData, email: e.target.value })}
-        />
-        <br />
-        <br />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                {/* <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                /> */}
 
-        <br />
-        <TextField
-          id="password"
-          label="password"
-          type="password"
-          color="secondary"
-          focused
-          margin="normal"
-          onChange={ e => setformData( {...formData , password: e.target.value} ) }
-        />
+                <TextField
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="email"
+                  label="email"
+                  type="email"
+                  color="secondary"
+                  focused
+                  margin="normal"
+                  onChange={(e) =>
+                    setformData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
+            </div>
 
-        <div className="d-flex justify-content-center">
-          <Button
-            onClick={handleFormSubmit}
-            margin="normal"
-            variant="contained"
-          >
-            Login
-          </Button>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                {/* <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                /> */}
+
+                <TextField
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="password"
+                  label="password"
+                  type="password"
+                  color="secondary"
+                  focused
+                  margin="normal"
+                  onChange={(e) =>
+                    setformData({ ...formData, password: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+
+            <div>
+              {/* <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button> */}
+
+              <Button
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleFormSubmit}
+                margin="normal"
+                variant="contained"
+              >
+                Login
+              </Button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Start a 14 day free trial
+            </a>
+          </p>
         </div>
       </div>
     </>
