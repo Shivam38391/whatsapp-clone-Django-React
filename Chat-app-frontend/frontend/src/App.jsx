b2b14a82f1220fc9caf0bd3bb102a8d0340a74b4
shivam sharma <shivamsharma38391@gmail.com>
2023-11-06 10:52:59 +0530

    Header footer and router is implemented

--- Chat-app-frontend/frontend/src/App.jsx
+++ Chat-app-frontend/frontend/src/App.jsx
@@ -1,14 +1,42 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import Register from './components/Register'
 import Login from './components/Login'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+import { BrowserRouter, Route , Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
-      {/* <Register/> */}
-      <Login/>
+
+<BrowserRouter>
+
+<Header/>
+
+
+<Routes>
+
+  <Route path='/login' element = {<Login/>} > </Route>
+  <Route path='/register' element = {<Register/>} > </Route>
+
+        {/* <Register/> */}
+        {/* // <Login/> */}
+
+</Routes>
+
+
+
+
+  <Footer/>
+
+
+</BrowserRouter>
+
+
     </>
   )
 }
